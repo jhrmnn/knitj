d5beeba3e7aba956794b596a83168cd170d369b6
Jan Hermann <dev@hermann.in>
2017-06-19 22:50:44 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='neptune',
+    version='0.1',
+    description='IPython frontend',
+    author='Jan Hermann',
+    author_email='dev@hermann.in',
+    url='https://github.com/azag0/neptune',
+    packages=['neptune'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+    license='Mozilla Public License 2.0',
+)
