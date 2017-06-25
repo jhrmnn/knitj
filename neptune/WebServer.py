a124fa966f0864ac82f02057b53864e6a0e09f2a
Jan Hermann <dev@hermann.in>
2017-06-25 09:51:54 +0200

    split away WebServer

---
+++ neptune/WebServer.py
@@ -0,0 +1,44 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from pathlib import Path
+from itertools import chain
+import asyncio
+
+from aiohttp import web
+import ansi2html
+from pygments.formatters import HtmlFormatter
+from jinja2 import Template
+
+from .Renderer import Renderer
+
+
+class WebServer:
+    def __init__(self, renderer: Renderer) -> None:
+        self.renderer = renderer
+        self._root = Path(__file__).parents[1]/'client'
+
+    def _get_response(self, text: str) -> web.Response:
+        return web.Response(text=text, content_type='text/html')
+
+    async def handler(self, request: web.BaseRequest) -> web.Response:
+        if request.path == '/':
+            return self._get_response(
+                Template((self._root/'templates/index.html').read_text()).render(
+                    cells=self.renderer.get_last_html(),
+                    styles='\n'.join(chain(
+                        [HtmlFormatter().get_style_defs()],
+                        map(str, ansi2html.style.get_styles())
+                    ))
+                )
+            )
+        try:
+            text = (self._root/'static'/request.path[1:]).read_text()
+        except FileNotFoundError:
+            raise web.HTTPNotFound()
+        return self._get_response(text)
+
+    async def run(self) -> None:
+        server = web.Server(self.handler)
+        loop = asyncio.get_event_loop()
+        await loop.create_server(server, '127.0.0.1', 8080)  # type: ignore
--- neptune/neptune.py
+++ neptune/neptune.py
@@ -1,55 +1,19 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from pathlib import Path
-from itertools import chain
 import asyncio
 
-import ansi2html
 import websockets
-from aiohttp import web
-from pygments.formatters import HtmlFormatter
-from jinja2 import Template
 
 from .Notebook import Notebook
 from .Renderer import Renderer
 from .Kernel import Kernel
 from .Source import Source
+from .WebServer import WebServer
 
 from typing import Set  # noqa
 
 WebSocket = websockets.WebSocketServerProtocol
-
-
-class WebServer:
-    def __init__(self, renderer: Renderer) -> None:
-        self.renderer = renderer
-        self._root = Path(__file__).parents[1]/'client'
-
-    def _get_response(self, text: str) -> web.Response:
-        return web.Response(text=text, content_type='text/html')
-
-    async def handler(self, request: web.BaseRequest) -> web.Response:
-        if request.path == '/':
-            return self._get_response(
-                Template((self._root/'templates/index.html').read_text()).render(
-                    cells=self.renderer.get_last_html(),
-                    styles='\n'.join(chain(
-                        [HtmlFormatter().get_style_defs()],
-                        map(str, ansi2html.style.get_styles())
-                    ))
-                )
-            )
-        try:
-            text = (self._root/'static'/request.path[1:]).read_text()
-        except FileNotFoundError:
-            raise web.HTTPNotFound()
-        return self._get_response(text)
-
-    async def run(self) -> None:
-        server = web.Server(self.handler)
-        loop = asyncio.get_event_loop()
-        await loop.create_server(server, '127.0.0.1', 8080)  # type: ignore
 
 
 async def neptune(path: str) -> None:
