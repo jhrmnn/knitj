60fb6e127200e6fb044e1882d2bcf234b9c5814b
Jan Hermann <dev@hermann.in>
2017-06-25 09:37:34 +0200

    split away Source

---
+++ neptune/Source.py
@@ -0,0 +1,87 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from pathlib import Path
+from itertools import cycle
+import hashlib
+import asyncio
+from asyncio import Queue
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
+
+from .Cell import Cell, Hash
+from .Renderer import Renderer, Document
+from .Kernel import Kernel
+from .jupyter_messaging.content import MIME
+
+from typing import Set  # noqa
+
+
+class FileChangedHandler(FileSystemEventHandler):
+    def __init__(self, queue: 'Queue[str]') -> None:
+        super().__init__()
+        self._loop = asyncio.get_event_loop()
+        self._queue = queue
+
+    def _queue_modified(self, event: FileSystemEvent) -> None:
+        self._loop.call_soon_threadsafe(self._queue.put_nowait, event.src_path)
+
+    def on_modified(self, event: FileSystemEvent) -> None:
+        self._queue_modified(event)
+
+    def on_created(self, event: FileSystemEvent) -> None:
+        self._queue_modified(event)
+
+
+def get_hash(s: str) -> Hash:
+    return Hash(hashlib.sha1(s.encode()).hexdigest())
+
+
+class Source:
+    def __init__(self, path: str, kernel: Kernel, renderer: Renderer) -> None:
+        self.path = Path(path)
+        self.kernel = kernel
+        self.renderer = renderer
+        self._queue: 'Queue[str]' = Queue()
+        self._observer = Observer()
+        self._observer.schedule(FileChangedHandler(queue=self._queue), '.')
+        self._last_inputs: Set[Hash] = set()
+
+    async def file_change(self) -> str:
+        while True:
+            file = Path(await self._queue.get())
+            if file == self.path:
+                return file.read_text()
+
+    def _parse(self, src: str) -> Document:
+        contents = src.split('```')
+        assert len(contents) % 2 == 1
+        cells = []
+        for kind, con in zip(cycle([Cell.Kind.TEXT, Cell.Kind.INPUT]), contents):
+            if kind == Cell.Kind.INPUT:
+                if con[:6] == 'python':
+                    con = con[6:]
+                else:
+                    con = f'```{con}```'
+                    kind = Cell.Kind.TEXT
+                mime = MIME.TEXT_PYTHON
+            else:
+                mime = MIME.TEXT_MARKDOWN
+            con = con.rstrip()
+            cells.append(Cell(kind, {mime: con}, get_hash(con)))
+        return cells
+
+    async def run(self) -> None:
+        self._observer.start()
+        while True:
+            src = await self.file_change()
+            document = self._parse(src)
+            self.renderer.add_task(document)
+            for cell in document:
+                if cell.kind == Cell.Kind.INPUT:
+                    if cell.hashid not in self._last_inputs:
+                        self.kernel.execute(cell)
+            self._last_inputs = set(
+                cell.hashid for cell in document if cell.kind == Cell.Kind.INPUT
+            )
--- neptune/neptune.py
+++ neptune/neptune.py
@@ -2,106 +2,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from pathlib import Path
-from itertools import cycle, chain
-import hashlib
+from itertools import chain
 import asyncio
-from asyncio import Queue
 
 import ansi2html
 import websockets
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, FileSystemEvent
 from aiohttp import web
 from pygments.formatters import HtmlFormatter
 from jinja2 import Template
 
-from .jupyter_messaging.content import MIME
-from .Cell import Cell, Hash
 from .Notebook import Notebook
-from .Renderer import Renderer, Document
+from .Renderer import Renderer
 from .Kernel import Kernel
+from .Source import Source
 
-from typing import (  # noqa
-    TYPE_CHECKING, Any, NewType, Set, Dict, Awaitable, Callable, List,
-    Union
-)
+from typing import Set  # noqa
 
 WebSocket = websockets.WebSocketServerProtocol
-
-
-if TYPE_CHECKING:
-    FileModifiedQueue = Queue[str]
-else:
-    FileModifiedQueue = None
-
-
-class FileChangedHandler(FileSystemEventHandler):
-    def __init__(self, queue: FileModifiedQueue) -> None:
-        super().__init__()
-        self._loop = asyncio.get_event_loop()
-        self._queue = queue
-
-    def _queue_modified(self, event: FileSystemEvent) -> None:
-        self._loop.call_soon_threadsafe(self._queue.put_nowait, event.src_path)
-
-    def on_modified(self, event: FileSystemEvent) -> None:
-        self._queue_modified(event)
-
-    def on_created(self, event: FileSystemEvent) -> None:
-        self._queue_modified(event)
-
-
-def get_hash(s: str) -> Hash:
-    return Hash(hashlib.sha1(s.encode()).hexdigest())
-
-
-class Source:
-    def __init__(self, path: str, kernel: Kernel, renderer: Renderer) -> None:
-        self.path = Path(path)
-        self.kernel = kernel
-        self.renderer = renderer
-        self._queue: FileModifiedQueue = Queue()
-        self._observer = Observer()
-        self._observer.schedule(FileChangedHandler(queue=self._queue), '.')
-        self._last_inputs: Set[Hash] = set()
-
-    async def file_change(self) -> str:
-        while True:
-            file = Path(await self._queue.get())
-            if file == self.path:
-                return file.read_text()
-
-    def _parse(self, src: str) -> Document:
-        contents = src.split('```')
-        assert len(contents) % 2 == 1
-        cells = []
-        for kind, con in zip(cycle([Cell.Kind.TEXT, Cell.Kind.INPUT]), contents):
-            if kind == Cell.Kind.INPUT:
-                if con[:6] == 'python':
-                    con = con[6:]
-                else:
-                    con = f'```{con}```'
-                    kind = Cell.Kind.TEXT
-                mime = MIME.TEXT_PYTHON
-            else:
-                mime = MIME.TEXT_MARKDOWN
-            con = con.rstrip()
-            cells.append(Cell(kind, {mime: con}, get_hash(con)))
-        return cells
-
-    async def run(self) -> None:
-        self._observer.start()
-        while True:
-            src = await self.file_change()
-            document = self._parse(src)
-            self.renderer.add_task(document)
-            for cell in document:
-                if cell.kind == Cell.Kind.INPUT:
-                    if cell.hashid not in self._last_inputs:
-                        self.kernel.execute(cell)
-            self._last_inputs = set(
-                cell.hashid for cell in document if cell.kind == Cell.Kind.INPUT
-            )
 
 
 class WebServer:
