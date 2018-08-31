90151f65d5c19c99fc4c582a62d527800925092d
Jan Hermann <dev@janhermann.cz>
2018-08-31 16:17:47 +0200

    remove knitj/logging.py

--- knitj/jupyter_messaging/message.py
+++ knitj/jupyter_messaging/message.py
@@ -7,7 +7,6 @@ from pprint import pformat
 from typing import NewType, Dict, Any, List
 
 from .content import content as cnt
-from ..logging import colstr
 
 UUID = NewType('UUID', str)
 
@@ -149,3 +148,31 @@ _msg_classes = {
 def parse(msg: Dict) -> BaseMessage:
     msg_type = MsgType(msg['msg_type'])
     return _msg_classes[msg_type](**msg)
+
+
+class colstr(str):
+    colors = {
+        'red': '\x1b[31m',
+        'green': '\x1b[32m',
+        'yellow': '\x1b[33m',
+        'blue': '\x1b[34m',
+        'bryellow': '\x1b[93m',
+        'brblue': '\x1b[94m',
+        'pink': '\x1b[35m',
+        'cyan': '\x1b[36m',
+        'grey': '\x1b[37m',
+        'normal': '\x1b[0m'
+    }
+
+    def __new__(cls, s: Any, color: str) -> str:
+        return str.__new__(  # type: ignore
+            cls,
+            colstr.colors[color] + str(s) + colstr.colors['normal']
+        )
+
+    def __init__(self, s: Any, color: str) -> None:
+        self.len = len(str(s))
+        self.orig = str(s)
+
+    def __len__(self) -> int:
+        return self.len
